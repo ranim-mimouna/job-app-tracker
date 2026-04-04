786a864dc2ed24412a9c4108c6a7076dbd64f8a4
Ranim Mimouna <ranim.mimouna26@gmail.com>
2026-04-05 00:31:47 +0100

    Initialize the backend project 869crzvfz

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+#job application tracker backend:
+how to run the server: npm run dev
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
